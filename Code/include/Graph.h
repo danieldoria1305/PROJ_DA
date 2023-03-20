8e8fb2694a3e896480e9b40a7d715490de75f660
gnlpc <up202103336@fe.up.pt>
2023-03-20 13:18:04 +0000

    added the graph and Vertex edge

---
+++ Code/include/Graph.h
@@ -0,0 +1,60 @@
+//
+// Created by gonca on 20/03/2023.
+//
+
+#ifndef PROJETODA_GRAPH_H
+#define PROJETODA_GRAPH_H
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <limits>
+#include <algorithm>
+#include <list>
+#include "VertexEdge.h"
+#include "LoadFiles.h"
+#include "Station.h"
+#include "Network.h"
+
+
+
+
+
+class Graph {
+
+    struct Edge{
+        string connectedStation;
+        int capacity;
+        string service;
+    };
+
+    struct Node{
+        string name;
+        list<Graph::Edge> adj;
+        bool visited = false;
+    };
+
+    int n; // graph size
+
+public:
+    Graph();
+
+    void addEdge (string name, string connectedStation, int capacity, string service);
+
+    void setVisitedFalse();
+
+    int getTotalStationCapacity(const string name);
+
+    int getNumberofTrainsService(const string service);
+
+    int totalOfTrainsInNetwork ();
+
+
+    void addNode(string name);
+
+};
+
+
+
+
+#endif //PROJETODA_GRAPH_H
---
+++ Code/src/Graph.cpp
@@ -0,0 +1,7 @@
+//
+// Created by gonca on 20/03/2023.
+//
+
+
+#include "../include/Graph.h"
+
