d1f5a45bda13c1616cde175647a101e993d33468
gnlpc <up202103336@fe.up.pt>
2023-04-10 19:34:07 +0100

    comments on the functions

--- Code/include/Functions.h
+++ Code/include/Functions.h
@@ -17,23 +17,21 @@
 #include <limits>
 #include <algorithm>
 
-
+/*
+ * Exercise 2.1
+ *
+ * The most amount trains that can travel between two stations (staA and staB)
+ *
+ * staA -> one of the stations
+ * staB -> the other station
+ *
+ *
+ */
+int maxNumTrainsTwoStations(string staA, string staB);
 
 void testAndVisit(std::queue< Vertex*> &q, Edge *e, Vertex *w, double residual);
 
 void edmondsKarp (string source, string dest);
-
-void edmondsKarpSameDistrict(string source, string dest);
-
-bool findAugmentingPathSameDistrict(Vertex *s, Vertex *t);
-
-int maxNumTrainsTwoStationsSameDistrict(string staA, string staB);
-
-void edmondsKarpSameMunicipality(string source, string dest);
-
-bool findAugmentingPathSameMunicipality(Vertex *s, Vertex *t);
-
-int maxNumTrainsTwoStationsSameMunicipality(string staA, string staB);
 
 bool findAugmentingPath(Vertex *s, Vertex *t);
 
@@ -41,17 +39,70 @@ double findMinResidualAlongPath(Vertex *s, Vertex *t);
 
 void augmentFlowAlongPath(Vertex *s, Vertex *t, double f);
 
-int maxNumTrainsTwoStations(string staA, string staB);
-
+/*
+ *Exercise 2.2
+ *
+ * Returns a vector with the sequence of stations where the most amount of trains can be simultaneously
+ *
+ *
+ */
 vector<pair<string, string>> maxMAxFlow();
 
+/*
+ *
+ * Exercise 2.3
+ *
+ * Returns a list with k elements. Each element represents a district and the number of trains in that district
+ *
+ * k -> the size of the list we want to see
+ *
+ */
 vector <pair<string, int>> maxFlowDistrict(int k);
 
-vector <pair<string, int>> maxFlowMunicipality(int k);
+int maxNumTrainsTwoStationsSameDistrict(string staA, string staB);
 
-int maxNumTrainsTwoStationsReduced(string staA, string staB, vector<Vertex*> vert);
+void edmondsKarpSameDistrict(string source, string dest);
+
+bool findAugmentingPathSameDistrict(Vertex *s, Vertex *t);
 
 /*
+ *
+ * Exercise 2.3
+ *
+ * Returns a list with k elements. Each element represents a Municipality and the number of trains in that municipality
+ *
+ * k -> the size of the list we want to see
+ *
+ */
+vector <pair<string, int>> maxFlowMunicipality(int k);
+
+int maxNumTrainsTwoStationsSameMunicipality(string staA, string staB);
+
+void edmondsKarpSameMunicipality(string source, string dest);
+
+bool findAugmentingPathSameMunicipality(Vertex *s, Vertex *t);
+
+/*
+ * Exercise 3.1
+ *
+ * The functions returns the minimum cost trip we can make between two stations (staA and staB) and its
+ * max flow. The max flow we can send from one station to another with the minimum cost.
+ *
+ * staA -> origin
+ * staB -> destination
+ */
+void leastCostPathAndMaxFlow(string staA, string staB);
+
+void dijkstraShortestPath(Vertex* source, Vertex* dest);
+
+
+/*
+ *
+ * Exercise 4.1
+ *
+ * After cutting the connection between two vertexes (staA and staB), the functions returns the max flow between two
+ * stations (staC and staD)
+ *
  * staA -> first station to remove
  * staB -> second station to remove
  * staA and staB should be adj from one another (validation needed in menu)
@@ -59,22 +110,24 @@ int maxNumTrainsTwoStationsReduced(string staA, string staB, vector<Vertex*> ver
  * staC -> origin
  * staD -> destination
  *
- * maxNumReducedConnectivity -> returns the max flow between staC and staD if we remove the edge that connects
- * staA and staB
  *
  */
 
 int maxNumReducedConnectivity (string staA,string staB, string staC, string staD);
 
-void dijkstraShortestPath(Vertex* source, Vertex* dest);
+int maxNumTrainsTwoStationsReduced(string staA, string staB, vector<Vertex*> vert);
 
-void leastCostPathAndMaxFlow(string staA, string staB);
+/*
+ * Exercise 4.2
+ *
+ *
+ * After removing the connections between two stations, we evaluate the max flow we can send from one station to another
+ *
+ * Remove connection between staA and staB
+ * evaluates max flow between staC and staD
+ */
 
 void report (string staA, string staB, string staC, string staD);
-
-int maxTrainsIncoming (string staA);
-
-
 
 
 #endif //PROJETODA_FUNCTIONS_H
